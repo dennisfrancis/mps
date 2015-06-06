6dd8d28f35bce99c22ebd2644a5cc5901feb4ce9
Dennis Francis <dennisfrancis.in@gmail.com>
2015-06-06 14:32:11 +0530

    add gz decompression

--- mps.go
+++ mps.go
@@ -3,7 +3,7 @@ package mps
 
 import (
 	"os"
-	//"compress/gzip"
+	"compress/gzip"
 	"io"
 )
 
@@ -11,15 +11,26 @@ import (
 
 func ParseMPS(fname string) (*LPData, error) {
 
-	rd, err := openMPSFile(fname)
+	rd, err := openMPSFile(fname, false)
 	if err != nil { return nil, err }
 	return mpsParser(rd)
 }
 
-func openMPSFile(fname string) (io.Reader, error) {
+func ParseCompressedMPS(fname string) (*LPData, error) {
+
+	rd, err := openMPSFile(fname, true)
+	if err != nil { return nil, err }
+	return mpsParser(rd)
+}
+
+
+func openMPSFile(fname string, isCompressed bool) (io.Reader, error) {
 	fp, err := os.Open(fname)
 	if err != nil { return nil, err }
-	//fp1, err1 := gzip.NewReader(fp)
-	//if err1 != nil { return fp, nil }
+	if isCompressed {
+		fp1, err1 := gzip.NewReader(fp)
+		if err1 != nil { return nil, err1 }
+		return fp1, nil
+	}
 	return fp, nil
 }
